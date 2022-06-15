c2c5771e86c1c7925e5189c2617ed91be0bb30a4
Ainur <ainura.abdenova@gmail.com>
2022-06-15 16:22:38 +0600

    NavLink isActive?

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,17 @@
-import { Link, Outlet } from 'react-router-dom'
-
+import { NavLink, Outlet } from 'react-router-dom'
 import React from 'react'
+
+const setActive = ({ isActive }) => isActive ? 'active-link' : '';
+
+
 
 export const Layout = () => {
     return (
         <div>
             <header>
-                <Link to='/'>Home</Link>
-                <Link to='/posts'>Blog</Link>
-                <Link to='/about'>About</Link>
+                <NavLink to='/' className={setActive}>Home</NavLink>
+                <NavLink to='/posts' className={setActive}>Blog</NavLink>
+                <NavLink to='/about' className={setActive}>About</NavLink>
             </header>
             <main className='container'>
                 <Outlet />
