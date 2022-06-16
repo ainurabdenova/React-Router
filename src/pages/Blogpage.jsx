9c7fb1f2049d5164500f4490909b651546e902ac
Ainur <ainura.abdenova@gmail.com>
2022-06-16 19:59:25 +0600

    useSearch

--- src/pages/Blogpage.jsx
+++ src/pages/Blogpage.jsx
@@ -1,12 +1,22 @@
 import { useEffect, useState } from 'react'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 
 
 
 export const Blogpage = () => {
 
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const postQuery = searchParams.get('post') || '';
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const query = form.search.value;
+        setSearchParams({ post: query })
+    }
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -18,9 +28,16 @@ export const Blogpage = () => {
 
     return (
         <div>
-            <h1>This is my posts</h1>
+            <h1>Our news</h1>
+            <form autoComplete='off' onSubmit={handleSubmit}>
+                <input type="search" name='search' />
+                <input type="submit" value="Search" />
+            </form>
+
             <Link to="/posts/new">Add new post</Link>
-            {posts.map((post) => (
+            {posts.filter(
+                post => post.title.includes(postQuery)
+            ).map((post) => (
                 <Link key={post.id} to={`/posts/${post.id}`}>
                     <li>{post.title}</li>
                 </Link>
